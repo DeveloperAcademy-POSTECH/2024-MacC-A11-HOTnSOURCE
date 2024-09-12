041608d3a165fa84faecd7bbafc54d71065cce08
eomchanu <djacksdn1@icloud.com>
2024-09-13 01:08:55 +0900

    [Chore] 프로젝트 생성 및 초기 설정

---
+++ Shh/Shh/App/ShhApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShhApp.swift
+//  Shh
+//
+//  Created by Eom Chanwoo on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ShhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
