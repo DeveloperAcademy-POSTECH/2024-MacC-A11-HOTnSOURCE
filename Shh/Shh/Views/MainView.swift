a8423af528de83167062baf5dde2d9621a76e646
eomchanu <djacksdn1@icloud.com>
2024-10-29 13:21:12 +0900

    Chore: MainView 오탈자 수정 (#57)
    routerManger -> routerManager

--- Shh/Shh/Views/MainView.swift
+++ Shh/Shh/Views/MainView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 // MARK: - 메인 뷰; 사용자의 소음 정도를 나타냅니다.
 struct MainView: View {
     // MARK: Properties
-    @EnvironmentObject var routerManger: RouterManager
+    @EnvironmentObject var routerManager: RouterManager
     
     @StateObject private var audioManager: AudioManager = {
         do {
@@ -51,7 +51,7 @@ struct MainView: View {
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
                 Button {
-                    routerManger.push(view: .meteringInfoView)
+                    routerManager.push(view: .meteringInfoView)
                 } label: {
                     Label("정보", systemImage: "info.circle")
                         .font(.title2)
@@ -208,7 +208,7 @@ struct MainView: View {
                         isStarted = true
                     } catch {
                         // TODO: 재생버튼 다시 눌러달라는 알러트 일단은 팝
-                        routerManger.pop()
+                        routerManager.pop()
                     }
                 } else {
                     audioManager.resumeMetering(place: selectedPlace)
@@ -233,7 +233,7 @@ struct MainView: View {
         Button {
             audioManager.stopMetering()
             isStarted = false // 시작 상태 초기화
-            routerManger.pop() // 정지 시 선택창으로 이동
+            routerManager.pop() // 정지 시 선택창으로 이동
         } label: {
             Image(systemName: "xmark")
                 .font(.title3)
