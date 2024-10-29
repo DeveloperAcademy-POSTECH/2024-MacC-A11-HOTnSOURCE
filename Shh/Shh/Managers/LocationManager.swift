e971188cc629f1f61902090352f3fdc13804b7ed
eomchanu <djacksdn1@icloud.com>
2024-10-29 14:46:35 +0900

    Bug: 선택 장소 비어있을 경우 고려 (#53)
    saveSelectedLocation(), decodeData() 함수 수정

--- Shh/Shh/Managers/LocationManager.swift
+++ Shh/Shh/Managers/LocationManager.swift
@@ -25,24 +25,23 @@ final class LocationManager: ObservableObject {
     
     // TODO: 워크스루 생성에 맞게 분리 필요
     func createLocation(_ location: Location) -> CreateLocationResult {
-        if locations.contains(where: { $0.name == location.name }) {
+        guard !locations.contains(where: { $0.name == location.name }) else {
             return .duplicateName
-        } else if locations.count > 5 {
+        }
+        
+        guard locations.count < 5 else {
             return .overCapacity
         }
         
         locations.append(location)
-        
         saveLocations()
         return .success
     }
     
     func editLocation(_ location: Location) {
-        guard let index = locations.firstIndex(where: { $0.id == location.id })
-        else { return }
+        guard let index = locations.firstIndex(where: { $0.id == location.id }) else { return }
         
         locations[index] = location
-        
         saveLocations()
     }
     
@@ -73,27 +72,37 @@ final class LocationManager: ObservableObject {
     private func saveSelectedLocation() {
         if let selectedLocation = selectedLocation {
             storedSelectedLocation = encodeData(selectedLocation) ?? ""
+        } else {
+            storedSelectedLocation = ""
         }
     }
     
     private func encodeData<T: Encodable>(_ data: T) -> String? {
-        guard let encodedData = try? JSONEncoder().encode(data), let jsonString = String(data: encodedData, encoding: .utf8)
-        else {
-            print("Failed to encode data")
+        do {
+            let encodedData = try JSONEncoder().encode(data)
+            return String(data: encodedData, encoding: .utf8)
+        } catch {
+            print("Failed to encode data:", error)
             return nil
         }
-        
-        return jsonString
     }
     
     private func decodeData<T: Decodable>(from jsonString: String) -> T? {
-        guard let data = jsonString.data(using: .utf8)
-        else {
-            print("Failed to decode data")
+        if jsonString.isEmpty {
             return nil
         }
         
-        return try? JSONDecoder().decode(T.self, from: data)
+        guard let data = jsonString.data(using: .utf8) else {
+            print("Failed to decode data: invalid string format")
+            return nil
+        }
+        
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            print("Failed to decode data:", error)
+            return nil
+        }
     }
 }
 
@@ -115,5 +124,4 @@ enum CreateLocationResult {
             return "알 수 없는 오류가 발생했습니다"
         }
     }
-
 }
