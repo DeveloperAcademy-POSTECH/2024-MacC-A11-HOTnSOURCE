ea0de0bf16520897b44461cd1ca850a23128c303
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-10-30 23:37:09 +0900

    Feat: 워치 앱 타겟 추가

---
+++ Shh/ShhWatch Watch App/ShhWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShhWatchApp.swift
+//  ShhWatch Watch App
+//
+//  Created by Jia Jang on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ShhWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
