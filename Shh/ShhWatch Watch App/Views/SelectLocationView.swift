273ca424704b76c26ff98f2b01284929f3143fb0
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-10-30 23:48:42 +0900

    Chore: 주석 표시 (#61)

--- Shh/ShhWatch Watch App/Views/SelectLocationView.swift
+++ Shh/ShhWatch Watch App/Views/SelectLocationView.swift
@@ -7,7 +7,11 @@
 
 import SwiftUI
 
+// MARK: - 장소 선택 뷰: iOS에서 가져온 장소 리스트를 보여줌
 struct SelectLocationView: View {
+    // MARK: Properties
+    
+    // MARK: Body
     var body: some View {
         VStack(spacing: 16) {
             Text("Select Location")
@@ -15,6 +19,8 @@ struct SelectLocationView: View {
         .navigationTitle("장소 선택")
         .scrollIndicators(.hidden)
     }
+    
+    // MARK: SubViews
 }
 
 #Preview {
