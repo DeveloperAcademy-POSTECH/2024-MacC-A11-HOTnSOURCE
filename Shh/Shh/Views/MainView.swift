1cbf2bd95b6757568f0550f986bf2c3930182ca0
frankwon11 <frankwon11@gmail.com>
2024-10-30 17:02:09 +0900

    Refactor: 메인 화면 구조 변경 (#68)

--- Shh/Shh/Views/MainView.swift
+++ Shh/Shh/Views/MainView.swift
@@ -23,18 +23,48 @@ struct MainView: View {
     
     // MARK: Body
     var body: some View {
-        ZStack {
-            content
+        VStack {
+            locationInfo
+            Spacer()
+            
+            // TODO: 대충 멋진 에셋
+            
+            HStack(alignment: .center) {
+                userNoiseStatusInfo
+                Spacer()
+                
+                meteringToggleButton
+            }
+            Spacer().frame(height: 20)
         }
+        .padding(.horizontal, 16)
         .navigationTitle(selectedLocation.name)
         .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                Button {
-                    routerManager.push(view: .meteringInfoView)
-                } label: {
-                    Label("정보", systemImage: "info.circle")
-                        .font(.title2)
-                        .fontWeight(.medium)
+            ToolbarItemGroup(placement: .topBarTrailing) {
+                HStack(alignment: .center) {
+                    Button {
+                        routerManager.push(view: .editLocationView(location: selectedLocation))
+                    } label: {
+                        Text("수정")
+                            .font(.body)
+                            .fontWeight(.regular)
+                    }
+                    
+                    Button {
+                        routerManager.push(view: .meteringInfoView)
+                    } label: {
+                        Label("정보", systemImage: "info.circle")
+                            .font(.body)
+                            .fontWeight(.regular)
+                    }
+                    
+                }
+            }
+        }
+        .onChange(of: audioManager.userNoiseStatus) { newValue in
+            Task {
+                if newValue == .caution {
+                    await notificationManager.sendNotification()
                 }
             }
         }
@@ -44,90 +74,31 @@ struct MainView: View {
     }
     
     // MARK: SubViews
-    private var content: some View {
-        VStack {
-            Spacer()
-            
-            if !audioManager.isMetering {
-                VStack(alignment: .center) {
-                    Text("아래 버튼을 눌러")
-                    Text("측정을 시작해주세요")
-                }
-                .font(.title2)
-                .fontWeight(.bold)
-                .foregroundStyle(.gray)
-                
-                Spacer()
-                Spacer()
-            }
-            
-            HStack {
-                if audioManager.isMetering {
-                    userNoiseStatusInfo
-                }
-                
-                Spacer()
-                
-                VStack {
-                    Spacer()
+    private var locationInfo: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("배경 소음 | \(Int(selectedLocation.backgroundDecibel)) dB")
                     
-                    VStack(spacing: 14) {
-                        if audioManager.isMetering {
-                            locationInfo
-                        } else {
-                            Spacer()
-                        }
-                        
-                        HStack {
-                            meteringToggleButton
-                            meteringStopButton
-                        }
-                        .navigationTitle(selectedLocation.name)
-                        .onChange(of: audioManager.userNoiseStatus) { newValue in
-                            Task {
-                                if newValue == .caution {
-                                    await notificationManager.sendNotification()
-                                }
-                            }
-                        }
-                    }
-                }
+                Text("측정 반경 | \(String(format: "%.1f", selectedLocation.distance)) m")
             }
-            .frame(height: 100)
+            .font(.body)
+            .fontWeight(.regular)
+            .foregroundStyle(.gray)
             
-            Spacer().frame(height: 40)
+            Spacer()
         }
-        .padding(.horizontal, 24)
     }
     
     private var userNoiseStatusInfo: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Text(audioManager.userNoiseStatus.message)
+        VStack(alignment: .leading, spacing: 10) {
+            Text(audioManager.isMetering ? audioManager.userNoiseStatus.message : "일시정지됨")
                 .font(.system(size: 56, weight: .bold, design: .default))
+                .fontWeight(.bold)
                 .foregroundStyle(.customWhite)
             
-            Text(audioManager.userNoiseStatus.writing)
+            Text(audioManager.isMetering ? audioManager.userNoiseStatus.writing : "측정을 다시 시작해주세요")
                 .font(.callout)
-                .fontWeight(.bold)
-                .foregroundStyle(.black)
-                .lineLimit(2, reservesSpace: true)
-        }
-    }
-    
-    private var locationInfo: some View {
-        HStack {
-            Text("\(Int(selectedLocation.backgroundDecibel)) dB")
-                .font(.body)
-                .fontWeight(.bold)
-                .foregroundStyle(.customWhite)
-            
-            Rectangle()
-                .fill(.customWhite)
-                .frame(width: 1, height: 18)
-            
-            Text("\(Int(selectedLocation.distance)) m")
-                .font(.body)
-                .fontWeight(.bold)
+                .fontWeight(.medium)
                 .foregroundStyle(.customWhite)
         }
     }
@@ -152,10 +123,11 @@ struct MainView: View {
             }
         } label: {
             Image(systemName: audioManager.isMetering ? "pause.fill" : "play.fill")
-                .font(.title3)
+                .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundStyle(.customWhite)
-                .padding()
+                .padding(.horizontal, 21)
+                .padding(.vertical, 16)
                 .background {
                     Circle()
                         .fill(.customBlack)
@@ -164,28 +136,10 @@ struct MainView: View {
         .accessibilityLabel(audioManager.isMetering ? "Pause metering" : "Resume metering")
         .accessibilityHint("Starts or pauses noise metering")
     }
-    
-    private var meteringStopButton: some View {
-        Button {
-            audioManager.stopMetering()
-            isStarted = false // 시작 상태 초기화
-            routerManager.pop() // 정지 시 선택창으로 이동
-        } label: {
-            Image(systemName: "xmark")
-                .font(.title3)
-                .fontWeight(.bold)
-                .foregroundStyle(.customWhite)
-                .padding()
-                .background {
-                    Circle()
-                        .fill(.customBlack)
-                }
-        }
-        .accessibilityLabel("Stop metering")
-        .accessibilityHint("Stop noise metering")
-    }
 }
 
 #Preview {
-    MainView(selectedLocation: Location(id: UUID(), name: "도서관", backgroundDecibel: 40.0, distance: 2.0))
+    NavigationStack {
+        MainView(selectedLocation: Location(id: UUID(), name: "도서관", backgroundDecibel: 40.0, distance: 2.0))
+    }
 }
