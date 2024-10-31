a6831b6f4026b23f6ebb97a9d2b69edec8c96767
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-11-01 03:10:30 +0900

    Feat: BackgroundDecibelMeteringButton 기능 달기 (#63)

--- Shh/ShhWatch Watch App/Views/Components/BackgroundDecibelMeteringButton.swift
+++ Shh/ShhWatch Watch App/Views/Components/BackgroundDecibelMeteringButton.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct BackgroundDecibelMeteringButton: View {
+    @EnvironmentObject var audioManager: AudioManager
+    
     @Binding var backgroundDecibel: Float
     @Binding var isShowingProgressView: Bool
     
@@ -17,7 +19,26 @@ struct BackgroundDecibelMeteringButton: View {
             .foregroundStyle(.green)
             .onTapGesture {
                 isShowingProgressView = true
-                // TODO: 소음 관련 로직이 들어갈 예정
+                // 주변 소음 측정을 시작
+                do {
+                    try audioManager.meteringBackgroundNoise { averageDecibel in
+                        // 측정된 평균 데시벨 값을 반올림하여 30, 35, 40, ..., 70 중 가장 가까운 값으로 저장
+                        let unRoundedAverageDecibel = averageDecibel
+                        
+                        // 5의 배수로 반올림 (예: 32.69 -> 35, 47.823 -> 50)
+                        let roundedDecibel = round(unRoundedAverageDecibel / 5.0) * 5.0
+                        
+                        // 30.0 ~ 70.0 사이로 범위를 제한
+                        let clampedDecibel = min(max(roundedDecibel, 30.0), 70.0)
+                        
+                        backgroundDecibel = clampedDecibel
+                        isShowingProgressView = false
+                    }
+                } catch {
+                    backgroundDecibel = 0
+                    isShowingProgressView = false
+                    print("소음 측정 중 오류 발생: \(error)")
+                }
             }
     }
 }
