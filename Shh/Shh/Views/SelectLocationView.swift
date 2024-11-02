875c012ea841d08268e2f6c33be176b0e9510f9c
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-11-02 22:16:03 +0900

    Refactor: 장소 생성 뷰 UXUI 업데이트 반영 (#81)

--- Shh/Shh/Views/SelectLocationView.swift
+++ Shh/Shh/Views/SelectLocationView.swift
@@ -15,21 +15,58 @@ struct SelectLocationView: View {
     
     // MARK: Body
     var body: some View {
-        VStack(spacing: 16) {
-            locationList
-            
-            createLocationButton
-        }
-        .navigationTitle("장소 선택")
-        .scrollIndicators(.hidden)
+        locationList
+            .navigationTitle("장소 선택")
+            .toolbar { createLocationButton }
+            .scrollIndicators(.hidden)
     }
     
     // MARK: Subviews
     private var locationList: some View {
-        ForEach(locationManager.locations.indices, id: \.self) { index in
-            let location = locationManager.locations[index]
+        List {
+            ForEach(locationManager.locations.indices, id: \.self) { index in
+                let location = locationManager.locations[index]
+                
+                locationButton(location)
+                    .listRowSeparator(.hidden)
+            }
+        }
+        .padding(.top, 20)
+    }
+    
+    private func locationButton(_ location: Location) -> some View {
+        Button {
+            routerManager.push(view: .mainView(selectedLocation: location))
+            locationManager.selectedLocation = location
+        } label: {
+            HStack {
+                locationButtonStyle(
+                    title: location.name,
+                    textColor: .white,
+                    bgColor: locationManager.selectedLocation?.id == location.id ? .green : .clear
+                )
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+                    .foregroundStyle(.white)
+                    .padding(.trailing, 10)
+            }
+        }
+    }
+    
+    private func locationButtonStyle(title: String, textColor: Color, bgColor: Color) -> some View {
+        HStack {
+            Capsule()
+                .fill(bgColor)
+                .frame(width: 8)
+                .padding(.vertical, 25)
+                .padding(.trailing, 10)
             
-            locationButton(location)
+            Text(title)
+                .font(.title3)
+                .fontWeight(.bold)
+                .foregroundStyle(textColor)
         }
     }
     
@@ -37,47 +74,10 @@ struct SelectLocationView: View {
         Button {
             routerManager.push(view: .createLocationView)
         } label: {
-            locationButtonStyle(title: "+", textColor: .white, bgColor: .gray)
-                .opacity(0.5)
+            Text("생성하기")
+                // TODO: 최대 생성 개수 10개로 변경 예정
+                .opacity(locationManager.locations.count >= 5 ? 0 : 1)
         }
-        .opacity(locationManager.locations.count >= 5 ? 0 : 1)
-    }
-    
-    private func locationButton(_ location: Location) -> some View {
-        ZStack(alignment: .trailing) {
-            Button {
-                routerManager.push(view: .mainView(selectedLocation: location))
-                locationManager.selectedLocation = location
-            } label: {
-                locationButtonStyle(
-                    title: location.name,
-                    textColor: .white,
-                    bgColor: locationManager.selectedLocation?.id == location.id ? .green : .gray
-                )
-            }
-            
-            Button {
-                routerManager.push(view: .editLocationView(location: location))
-            } label: {
-                Image(systemName: "ellipsis")
-                    .foregroundStyle(.white)
-                    .padding()
-                    .contentShape(Rectangle())
-            }
-        }
-    }
-    
-    private func locationButtonStyle(title: String, textColor: Color, bgColor: Color) -> some View {
-        Text(title)
-            .font(.title3)
-            .fontWeight(.bold)
-            .foregroundStyle(textColor)
-            .padding(.vertical, 20)
-            .frame(width: 300)
-            .background(
-                RoundedRectangle(cornerRadius: 100)
-                    .fill(bgColor)
-            )
     }
 }
 
