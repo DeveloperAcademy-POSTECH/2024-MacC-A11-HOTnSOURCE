894a094462fa939e104f218ba99b0b97f008ecc9
frankwon11 <frankwon11@gmail.com>
2024-11-16 01:49:35 +0900

    Chore: 임시 MainView 생성 (#116)

---
+++ Shh/Shh/Views/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  MainView.swift
+//  Shh
+//
+//  Created by sseungwonnn on 11/16/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        // TODO: 디자인 예정
+        Text("진짜 메인뷰")
+    }
+}
+
+#Preview {
+    MainView()
+}
