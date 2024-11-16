f9c2cea9f15acc1b7471e2ce04a1223f78b73814
frankwon11 <frankwon11@gmail.com>
2024-11-16 20:56:15 +0900

    Fix: 배경 소음 측정 로직 개선 (#118)
    튀는 값이 발생하면, 예외를 던짐

--- Shh/Shh/Views/MainView.swift
+++ Shh/Shh/Views/MainView.swift
@@ -9,15 +9,29 @@ import SwiftUI
 
 struct MainView: View {
     @EnvironmentObject var router: Router
+    @EnvironmentObject var audioManager: AudioManager
     
+    @State private var backgroundNoise: Float = 20.0
+    @State private var isShowingProgressView: Bool = false
     var body: some View {
         // TODO: 디자인 예정
         VStack {
             Text("진짜 메인뷰")
+                .font(.title)
+            BackgroundDecibelMeteringButton(backgroundDecibel: $backgroundNoise, isShowingProgressView: $isShowingProgressView)
             
+            Text("배경소음: \(backgroundNoise)")
             CustomButton(text: "측정 뷰로 이동") {
                 router.push(view: .meteringView)
             }
+        }
+        .onAppear {
+            do {
+                try audioManager.setAudioSession()
+            } catch {
+                print("oops")
+            }
+            
         }
     }
 }
