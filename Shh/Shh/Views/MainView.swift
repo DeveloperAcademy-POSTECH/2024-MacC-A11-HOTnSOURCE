8b5ef3660ec7d17cbad50f2be556ce3ce0919ffe
frankwon11 <frankwon11@gmail.com>
2024-11-18 17:43:08 +0900

    Fix: 배경 소음 측정 로직 개선 (#131)

--- Shh/Shh/Views/MainView.swift
+++ Shh/Shh/Views/MainView.swift
@@ -9,12 +9,23 @@ import SwiftUI
 
 struct MainView: View {
     @EnvironmentObject var router: Router
+    @EnvironmentObject var audioManager: AudioManager
     
     var body: some View {
         // TODO: 디자인 예정
         VStack {
             Text("진짜 메인뷰")
-            
+            Button {
+                Task {
+                    do {
+                        try await audioManager.meteringBackgroundNoise()
+                    } catch {
+                        print("웁스")
+                    }
+                }
+            } label: {
+                Text("배경 소음 측정")
+            }
             CustomButton(text: "측정 뷰로 이동") {
                 router.push(view: .meteringView)
             }
